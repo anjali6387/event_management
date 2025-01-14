eef043f43432c12d2c8cc4f787cc702e810f7bb3
Anjali Gupta <110262694+anjali6387@users.noreply.github.com>
2025-01-15 03:06:37 +0530

    Update index.js

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cookieParser());
 // app.use(cors({origin: "http://localhost:5173/",credentials: true}));
-const allowedOrigins = ['http://localhost:5173'];
+const allowedOrigins = ['http://localhost:5173','https://event-management-ebon-zeta.vercel.app/'];
 app.use(cors({
   origin: allowedOrigins, // Frontend origin
   credentials: true, // Allow cookies and headers
