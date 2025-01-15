961e9c53f34861120d65a451d61a8e2577629186
Anjali Gupta <110262694+anjali6387@users.noreply.github.com>
2025-01-15 23:50:00 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 // app.use(cors({origin: "http://localhost:5173/",credentials: true}));
 const allowedOrigins = ['http://localhost:5173','https://event-management-ebon-zeta.vercel.app/'];
 app.use(cors({
-  origin: 'https://event-management-ebon-zeta.vercel.app', // Frontend origin
+  origin: true, // Frontend origin
    methods: ["GET", "POST","PUT","DELETE"],
   credentials: true, // Allow cookies and headers
 }));
