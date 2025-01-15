bf860d2bb908b7fa47dbd849f358f7c7c726c2da
Anjali Gupta <110262694+anjali6387@users.noreply.github.com>
2025-01-15 13:46:00 +0530

    Update axios.js

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development"?  "https://event-backend-pfus.onrender.com":"/api",
+    baseURL: import.meta.env.MODE === "development"?  "https://event-backend-pfus.onrender.com/api":"/api",
     withCredentials: true,
 })
